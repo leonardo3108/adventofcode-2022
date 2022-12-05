0069c01c54e5cf40981d6c4a7046b48bd590af42
leonardo3108 <leonardo3108@gmail.com>
2022-12-05 20:45:21 -0300

    Day 5 - https://adventofcode.com/2022/day/5

--- day05.py
+++ day05.py
@@ -22,21 +22,40 @@ for line in open('input05.txt'):
         fields = line.split()
         instructions.append([fields[3], fields[5], int(fields[1])])
 stacks = {}
+stacks_two = {}
 
 for stack_number, stack_name in enumerate(stack_names):
     stacks[stack_name] = deque()
+    stacks_two[stack_name] = deque()
     for line in grid[::-1]:
         cell = line[stack_number]
         if cell:
             stacks[stack_name].append(cell)
-#print(stacks)
-for stack_from, stack_to, move_quantity in instructions:
-    #print(stacks[stack_from], '=', move_quantity, '=>', stacks[stack_to])
-    for move_number in range(move_quantity):
-        stacks[stack_to].append(stacks[stack_from].pop())
+            stacks_two[stack_name].append(cell)
+
+def make_moves(instructions, stacks, multiple, stack_names):
+    for stack_from, stack_to, move_quantity in instructions:
+        #print(stacks[stack_from], '=', move_quantity, '=>', stacks[stack_to])
+        moved_multiple = deque()
+        for _ in range(move_quantity):
+            moved_one = stacks[stack_from].pop()
+            if multiple:
+                moved_multiple.append(moved_one)
+            else:
+                stacks[stack_to].append(moved_one)
+        if multiple:
+            for _ in range(move_quantity):
+                moved_one = moved_multiple.pop()
+                stacks[stack_to].append(moved_one)
         #print('\t', stacks[stack_from], '==>', stacks[stack_to])
-    
-top_crates = ''.join([stacks[stack_name].pop() for stack_name in stack_names])
+    return ''.join([stacks[stack_name].pop() for stack_name in stack_names])
+
+#print(stacks)
+top_crates = make_moves(instructions, stacks, False, stack_names)
 print('Part one:', top_crates)
+
+#print(stacks_two)
+top_crates = make_moves(instructions, stacks_two, True, stack_names)
+print('Part two:', top_crates)
 
 
