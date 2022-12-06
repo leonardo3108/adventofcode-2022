fcf4933b50923bd3847e380993a55abd4cd7a1dc
leonardo3108 <leonardo3108@gmail.com>
2022-12-06 14:03:50 -0300

    Day 6 - https://adventofcode.com/2022/day/6

--- day06.py
+++ day06.py
@@ -1,12 +1,20 @@
+from collections import Counter
+
 for line in open('input06.txt'):
     pass
 packet = line.rstrip()
 #print(packet)
-for start_position in range(len(packet)-3):
-    end_position = start_position + 4
-    marker = packet[start_position:end_position]
-    check = sorted(marker)
-    if check[0] != check[1] and check[1] != check[2] and check[2] != check[3]:
-        break
-    #else:        print(end_position, marker, 'nok')
-print('Part one:', end_position, '(' + marker + ')')
+
+def find_marker(packet, size):
+    for start_position in range(len(packet)):
+        end_position = start_position + size
+        marker = packet[start_position:end_position]
+        assert len(marker) == size
+        if Counter(marker).most_common()[0][1] == 1:   #[0] get the most common item, [1] the quantity; if it is 1, every characters are distinct, so it is the marker
+            break
+    return end_position, marker
+
+position, marker = find_marker(packet, 4)
+print('Part one:', position, '(' + marker + ')')
+position, marker = find_marker(packet, 14)
+print('Part two:', position, '(' + marker + ')')
